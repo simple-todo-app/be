02ce8280c45f06f8f9040796ecb196ef2d1ec2d7
Dylan Baker <dylanmbaker@yahoo.com>
2022-06-30 00:39:13 -0500

    added email verification

--- api/auth/authMiddle.js
+++ api/auth/authMiddle.js
@@ -6,7 +6,9 @@ const checkEmail = async (req, res, next) => {
     const { email } = req.body;
     const emailLowercase = email.toLowerCase();
     const [user] = await getUserBy({ email: emailLowercase });
-    if (!user) {
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+      res.json({ message: 'Please enter a valid email.' })
+    } else if (!user) {
       next();
     } else {
       res.status(401).json({ message: 'A user with that email already exists.' })
